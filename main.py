001f2e26d2047c4f683d007bad5a7fd4230ae604
carterbancroft <carter.bancroft@gmail.com>
2025-10-10 20:44:32 -0400

    Small cleanup

--- main.py
+++ main.py
@@ -13,18 +13,15 @@ s_height = 640
 screen = pygame.display.set_mode((s_width, s_height))
 clock = pygame.time.Clock()
 
-running = True
-dt = 0
-
 level_data_path = os.path.join("data", "levels", "level_1.json")
 level = Level(level_data_path)
 renderer = Renderer(screen, level.data["tile_size"])
 input_handler = InputHandler()
-
+player = Player(level.data["spawn_point"]["x"], level.data["spawn_point"]["y"])
 
 FPS = 60
-
-player = Player(level.data["spawn_point"]["x"], level.data["spawn_point"]["y"])
+delta = 0
+running = True
 
 while running:
     # Get delta time in seconds
